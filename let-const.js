f948345a34c20972c75a7e43f4e6d8f8dfbcc2b1
aarifislam8494-star <aarifislam8494@gmail.com>
2026-02-26 23:58:31 +0600

    first commit

---
+++ let-const.js
@@ -0,0 +1,20 @@
+const name = 'Bangladesh';
+// name = 'India';
+
+const cunteryName = 'BD_' + name;
+console.log(cunteryName);
+
+let price = 120;
+price = 150;
+console.log(price)
+
+const deress = ['shirt', 'pant', 'shoe'];
+// deress = ['shirt', 'pant', 'shoe', 'Belt'];
+deress.push('Belt');
+console.log(deress);
+
+student =  {
+ name: 'arif',
+ age: 22,
+ city: 'Bangladesh'
+}
